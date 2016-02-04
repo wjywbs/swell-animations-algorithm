22db3eb9f341069a784a39151b532e41755f2843
Carlo Rosati <carlo.rosati@drexel.edu>
2016-02-04 01:01:50 -0500

    Move everything out of main. Pass parameters by reference.

--- hermite/hermite.c
+++ hermite/hermite.c
@@ -1,31 +1,38 @@
-int main(int argc, char **argv)
+#include <stdio.h>
+
+struct pt {
+	double x;
+	double y;
+};
+
+struct pt minus(struct pt *l, struct pt *r)
 {
-	struct pt {
-		double x;
-		double y;
+	struct pt diff = {
+		.x = l->x - r->x,
+		.y = l->y - r->y
 	};
+	return diff;
+}
 
-	struct pt minus(struct pt l, struct pt r)
-	{
-		struct pt diff = {
-			.x = l.x - r.x,
-			.y = l.y - r.y
-		};
-		return diff;
-	}
+struct pt mult(struct pt *l, struct pt *r)
+{
+	struct pt mult = {
+		.x = l->x * r->x,
+		.y = l->y * r->y
+	};
+	return mult;
+}
 
-	struct pt mult(struct pt l, struct pt r)
-	{
-		struct pt mult = {
-			.x = l.x * r.x,
-			.y = l.y * r.y
-		};
-		return mult;
-	}
-
-	struct pt diff1(struct pt a, struct pt b)
-	{
+struct pt diff1(struct pt *a, struct pt *b)
+{
 /* TODO(Make this work)
 		return mult(3.0, minus(a,b));*/
-	}
+}
+
+int main(int argc, char **argv)
+{
+	struct pt a = {.x=1.0,.y=1.0};
+	struct pt b = {.x=2.0,.y=3.0};
+	struct pt c = minus(&a,&b);
+	printf("(%f,%f)\n",c.x,c.y);
 }
