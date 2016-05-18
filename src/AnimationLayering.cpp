df1b2c0dba0dead449de1012afc8f4da65d1661a
Neil Castelino <neil.fl93@gmail.com>
2016-05-17 23:54:25 -0400

    added changes to layering

--- src/AnimationLayering.cpp
+++ src/AnimationLayering.cpp
@@ -6,8 +6,10 @@
 #include <vector>
 #include <math.h>
 #include "point.h"
+#include "modeldata.pb.h"
 
 using namespace std;
+using namespace swellanimations;
 
 /*
  * Disposable (for debug purposes): Reads "points"
@@ -42,8 +44,16 @@ double GetAngleBetweenVectors(pt a, pt b) {
 	return atan2(b.y - a.y, b.x - a.x);
 }
 
+double GetAngleBetweenVectors(Vector a, Vector b) {
+	return atan2(b.y() - a.y(), b.x() - a.x());
+}
+
 double DistanceBetweenPoints(pt a, pt b) {
 	    return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2));
+}
+
+double DistanceBetweenPoints(Vector a, Vector b) {
+	return sqrt( pow(b.x() - a.x(), 2) + pow(b.y() - a.y(), 2) + pow(b.z() - a.z(), 2) );
 }
 
 pt MovePoint(pt a_spline, pt b_spline, pt a_model, pt b_model) {
@@ -60,7 +70,25 @@ pt MovePoint(pt a_spline, pt b_spline, pt a_model, pt b_model) {
 	return new_variant;
 }
 
-vector<pt> Morph(vector<pt> detail, vector<pt> model) {
+/*
+ * Moves a vector from sequence { a_model, b_model, c_model } to conform to the
+ * curve given by { a_spline, b_spline, c_spline }
+ */
+void MoveVector(Vector* a_model, Vector* b_model, Vector* a_spline, Vector* b_spline, int dist_to_dloa) {
+	double theta_spline = GetAngleBetweenVectors(*a_spline, *b_spline);
+	double dist_spline  = DistanceBetweenPoints(*a_spline, *b_spline);
+	double dist_model   = DistanceBetweenPoints(*a_model, *b_model);
+
+	double ratio = dist_model/dist_spline;
+
+	double new_x = b_model->x() + (cos(theta_spline)/(dist_to_dloa == 0? 1 : (double)dist_to_dloa));
+	double new_y = b_model->y() + (sin(theta_spline)/(dist_to_dloa == 0? 1 : (double)dist_to_dloa));
+
+	b_model->set_x(new_x);
+	b_model->set_y(new_y);
+}
+
+/*vector<pt> Morph(vector<pt> detail, vector<pt> model) {
 	vector<int>control;
 	control.push_back(0);
 	control.push_back(detail.size()*34/100);
@@ -76,7 +104,67 @@ vector<pt> Morph(vector<pt> detail, vector<pt> model) {
 	}
 
 	return model;
+}*/
+
+void Morph(Node *frames, AnimationLayer* layer, int dloa_size, int dist_to_dloa) {
+	Vector *current_position = frames->mutable_position();
+	Node *nextchild = frames->mutable_children(0);
+	int childcount = 0;
+	int childrenleft = 0;
+
+	// Find number of points from child root
+	Node *temp = nextchild;
+	while(temp->children_size() > 0) {
+		childcount++;
+	}
+	childrenleft = childcount;
+	temp = NULL;
+
+	// Morph first point from root position
+	MoveVector(	current_position,
+			nextchild->mutable_position(),
+			layer->mutable_layerpoints(0),
+			layer->mutable_layerpoints((dloa_size/100)*(100*1/(childcount))),
+			dist_to_dloa
+			);
+
+	current_position = nextchild->mutable_position();
+	nextchild = nextchild->mutable_children(0);
+
+	// Next couple points on the model's spine
+	for(int x = 1; nextchild->children_size() > 0; x++) {
+		MoveVector(	current_position,
+				nextchild->mutable_position(),
+				layer->mutable_layerpoints((dloa_size/100)*(100*(x-1)/childcount)),
+				layer->mutable_layerpoints((dloa_size/100)*(100*x/(childcount))),
+				dist_to_dloa
+				);
+		current_position = nextchild->mutable_position();
+		nextchild = nextchild->mutable_children(0);
+
+		// Just in case we have a infinite loop:
+		if(x>20)
+			break;
+	}
+
+	
 }
+
+void AddLayering(ModelData *modelData, Animation *animation) {
+	for(int i=0; i < modelData->animationlayers_size(); i++) {
+		for(int j = modelData->animationlayers(i).startframe();
+		    j < modelData->animationlayers(i).startframe() +
+		   	 modelData->animationlayers(i).numframes();
+		    j++) {
+			Morph(	animation->mutable_frames(j),
+				modelData->mutable_animationlayers(i),
+				modelData->animationlayers(i).layerpoints_size(),
+				modelData->animationlayers(i).startframe() + modelData->animationlayers(i).numframes() - j
+			     );
+		} // end frame counter
+	} // end layer counter
+}
+
 
 /*
 int main() {
--- src/swell-integration.cpp
+++ src/swell-integration.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "swell-integration.h"
 #include "AnimationGeneration.cpp"
+#include "AnimationLayering.cpp"
 #include "modeldata.pb.h"
 
 using namespace swellanimations;
@@ -24,6 +25,10 @@ void* generateAnimation(char* a, int size, unsigned int& responseSize) {
     modelData->ParseFromArray(a, size);
     //Animation* animation = generateTestData(modelData);
     Animation* animation = getFrames(modelData);
+
+    //Call layering
+    AddLayering(modelData, animation);
+
     responseSize = animation->ByteSize();
     void* response = malloc(responseSize);
     animation->SerializeToArray(response, responseSize);
